758367ca3a853812e33ab84680543280e0bd2f89
umangjpatel <umangpatel1947@gmail.com>
2018-10-29 22:02:37 +0530

    Build revised

---
+++ app/src/main/java/io/github/umangjpatel/gallop/info/CourseInfoActivity.java
@@ -0,0 +1,15 @@
+package io.github.umangjpatel.gallop.info;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+import io.github.umangjpatel.gallop.R;
+
+public class CourseInfoActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_course_info);
+    }
+}
